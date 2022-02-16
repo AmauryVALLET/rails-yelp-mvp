b893f2c8e4535c085161bfb3254905cc1156f444
Amaury Vallet <amaury.vallet@gmail.com>
2022-02-16 16:42:11 +0100

    exo 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+dish1 = Restaurant.create(name: "dish1", address: "1 Boundary St, London E2 7JE", category: "chinese")
+dish2 = Restaurant.create(name: "dish2", address: "2 Boundary St, Paris E2 7JE", category: "italian")
+dish3 = Restaurant.create(name: "dish3", address: "3 Boundary St, Madird E2 7JE", category: "japanese")
+dish4 = Restaurant.create(name: "dish4", address: "4 Boundary St, Berlin E2 7JE", category: "japanese")
+dish5 = Restaurant.create(name: "dish5", address: "5 Boundary St, Bruxelles E2 7JE", category: "french")
+
+review = Review.create(rating: 2, content: "ain’t good")
+review1 = Review.create(rating: 1, content: "quite good ")
+review2 = Review.create(rating: 4, content: "quite good ")
